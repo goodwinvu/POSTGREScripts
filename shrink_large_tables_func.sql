87d20bc03fc2cae8696d778026e4743a7e3fa9b8
v.belyaev <v.belyaev@1ceasy.ru>
2023-09-06 15:34:47 +0300

    ref

--- shrink_large_tables_func.sql
+++ shrink_large_tables_func.sql
@@ -5,10 +5,17 @@ DECLARE
 		tsize text;
         recindx text;
         beg_tsize text;
+		short_name text;
 		big_size int;
+		num_row_save int;
 		sDate timestamp;
 		only_test bool;
-BEGIN	
+BEGIN
+	big_size := 20971520; -- указываем размер после которого таблица считается большй 20мб по умолчанию
+	sDate := '2023-08-01'; -- указываем период до которого обрезаем периодические таблицы
+   	num_row_save := 100; -- количество строк оставляемых в непериодических таблицах
+	only_test = true; -- тестовый прогон - true - не трогать данные, только сформировать выборки, false - удаление данных
+	
 	RAISE NOTICE 'Очистка больших таблиц.Начало выполнения.
     ';
 
@@ -46,21 +53,29 @@ BEGIN
 		EXECUTE 'SELECT pg_size_pretty( pg_table_size( '''|| mviews.table_name ||''' ) )' Into beg_tsize;
         RAISE NOTICE 'Начальный размер таблицы % ', beg_tsize;
 		-- Очистка таблиц
-        rectext := 'DELETE FROM ' || mviews.table_name || ' WHERE ' || mviews.column_name || ' < timestamp '''||sDate||'''';  
 		IF  mviews.column_name IS NULL 
 			THEN
-				rectext := 'TRUNCATE ' || mviews.table_name;    
+				--rectext := 'TRUNCATE ' || mviews.table_name; раньше удаляли целиком  
+				EXECUTE 'SELECT replace('''|| mviews.table_name ||''', ''"'', '''')'  Into short_name;
+				rectext := 'DELETE FROM ' || mviews.table_name || '	WHERE ctid IN (SELECT ctid FROM ' || mviews.table_name || ' ORDER BY ctid asc
+                         LIMIT (
+									SELECT reltuples::bigint AS estimate 
+									FROM   pg_class WHERE  oid = '''|| short_name ||'''::regclass) - ' || num_row_save || ')'; -- теперь оставляем 100 записей
+			ELSE
+			    rectext := 'DELETE FROM ' || mviews.table_name || ' WHERE ' || mviews.column_name || ' < timestamp '''||sDate||''''; 
 		END IF;
-		--RAISE NOTICE 'Выполняется очистка %',rectext;		
-		IF only_test = false THEN
+				
+		IF only_test = true THEN
+				RAISE NOTICE 'Выполнится очистка %',rectext;
+		ELSE
 			BEGIN
 				EXECUTE (rectext);
 					-- Подсчет нового места
 				EXECUTE 'SELECT pg_size_pretty( pg_table_size( (tb.table_schema||''.''||tb.table_name ) )) 
 							FROM information_schema.tables AS tb
 							WHERE  tb.table_name = split_part(replace( '''||mviews.table_name||''', ''"'', ''''),''.'',2)' Into tsize;
-				RAISE NOTICE 'Очищена таблица % по условию период записи % < %, Конечный размер % .',
-				mviews.table_name, mviews.column_name, sDate, tsize;	
+				RAISE NOTICE 'Очищена таблица % по условию период записи % < %, нач.размер % - итог.размер % .',
+				mviews.table_name, mviews.column_name, sDate, beg_tsize, tsize;	
 		
 			EXCEPTION WHEN OTHERS 
 				THEN
@@ -72,19 +87,16 @@ BEGIN
 		END IF;	
 		
 		-- Очистка индексов
-		RAISE NOTICE 'Очистка индексов таблицы % ...', mviews.table_name;
 		IF only_test = false THEN
+			RAISE NOTICE 'Очистка индексов таблицы % ...', mviews.table_name;
+
 			BEGIN
-				EXECUTE (
-				SELECT 'DROP INDEX ' || string_agg(indexrelid::regclass::text, ', ')
+				EXECUTE (SELECT 'DROP INDEX ' || string_agg(indexrelid::regclass::text, ', ')
 				FROM   pg_index  i
-				WHERE  i.indrelid = mviews.table_name::regclass 
-				) Into recindx;
-				RAISE NOTICE 'Таблица % очищена
-				', mviews.table_name;
+				WHERE  i.indrelid = mviews.table_name::regclass );
+				RAISE NOTICE 'Таблица % очищена ', mviews.table_name;
 			EXCEPTION WHEN null_value_not_allowed THEN
-                        RAISE NOTICE 'Индекса таблицы % уже очищен
-                        ', mviews.table_name;
+                        RAISE NOTICE 'Индекса таблицы % была очищена ранее', mviews.table_name;
                       WHEN OTHERS THEN
 				        RAISE NOTICE 'ERROR CODE: %. MESSAGE TEXT: %', SQLSTATE, SQLERRM;
 				        RAISE NOTICE 'Ошибка очистки индекса таблицы % 
